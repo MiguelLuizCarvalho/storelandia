551fd48735f02e512cad0c561698f33e652807ce
Miguel Luiz Ferreira de Carvalho <miguelluiz@192.168.0.7>
2026-03-28 19:46:35 -0300

    feat: register auth blueprint and cleanup run.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ def create_app():
     app = Flask(__name__)
 
     from .routes import main
+    from .auth import auth_bp
+
     app.register_blueprint(main)
+    app.register_blueprint(auth_bp)
 
     return app
