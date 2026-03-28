c615912d06f94f44f4f36b37457d88f207334593
Miguel Luiz Ferreira de Carvalho <miguelluiz@192.168.0.7>
2026-03-28 20:07:58 -0300

    fix: use create_app instead of manual flask instance again...

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask
-
 from app import create_app
 
-app = Flask(__name__, 
-            template_folder='app/templates', 
-            static_folder='app/static')
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
