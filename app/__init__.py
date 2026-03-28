7238a4bcaf2b313c46d8b8b9410d4adb92cff27e
Miguel Luiz Ferreira de Carvalho <miguelluiz@192.168.0.7>
2026-03-28 20:05:58 -0300

    I changed the app.py and __init__.py code

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, 
+                template_folder='templates', 
+                static_folder='static')
 
     from .routes import main
     from .auth import auth_bp
-
+    
     app.register_blueprint(main)
     app.register_blueprint(auth_bp)
 
